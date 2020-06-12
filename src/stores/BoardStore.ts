b37188f06c6d11882a84b33a518255d127d4f66c
Alex Guo <alexqguo@gmail.com>
2020-06-11 23:46:41 -0400

    This shouldn't even be a commit

--- src/stores/BoardStore.ts
+++ src/stores/BoardStore.ts
@@ -1,6 +1,6 @@
 import { BoardSchema } from 'src/types';
 
-// Technically has no reason to use mobx
+// Technically has no reason to use mobx right now
 // It's all local and static data
 export default class BoardStore {
   boardSchema: BoardSchema = {
@@ -9,6 +9,9 @@ export default class BoardStore {
   };
 
   setBoardSchema = (schema: BoardSchema) => {
+    if (this.boardSchema.tiles.length) throw new Error('Schema already initialized');
+
+    // Do mutations or whatever here
     this.boardSchema = schema;
   }
 }
