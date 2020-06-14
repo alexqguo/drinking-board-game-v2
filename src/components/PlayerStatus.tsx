8264a6c73575562412c285674d49d6a7ead255ff
Alex Guo <alexguo@amazon.com>
2020-06-14 11:55:40 -0400

    Small change to status to use correct component

--- src/components/PlayerStatus.tsx
+++ src/components/PlayerStatus.tsx
@@ -4,6 +4,7 @@ import { Pane, Heading, Button, Text } from 'evergreen-ui';
 import { Player, GameState } from 'src/types';
 import { TranslationContext } from 'src/providers/TranslationProvider';
 import { StoreContext } from 'src/providers/StoreProvider';
+import DiceRoll from 'src/components/DiceRoll';
 
 export default () => {
   const i18n = useContext(TranslationContext);
@@ -12,6 +13,9 @@ export default () => {
   const isActionable = gameStore.isMyTurn && gameStore.game.state === GameState.ROLL_START;
   
   const skipTurn = () => gameStore.setGameState(GameState.TURN_SKIP);
+  const onRoll = (rolls: number[], reset: Function) => {
+    console.log(rolls, reset);
+  };
 
   return useObserver(() => (
     <Pane
@@ -25,13 +29,7 @@ export default () => {
       left={0}
     >
       <Heading>{player.name}</Heading>
-      <Button 
-        disabled={!isActionable}
-        iconBefore="hand-right"
-        appearance="primary"
-      >
-        {i18n.playerStatus.roll}
-      </Button>
+      <DiceRoll disabled={!isActionable} onRoll={onRoll} />
       <Button 
         disabled={!isActionable}
         iconBefore="disable"
